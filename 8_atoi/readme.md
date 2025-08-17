352c1c4ff17d45a96b3ae2a83f33dd716f7986cf
Isaac Barbosa <isaaccezar22@gmail.com>
2025-08-17 20:53:41 +0100

    Adds '8.atoi' with readme description.

---
+++ 8_atoi/readme.md
@@ -0,0 +1,9 @@
+# String to Integer (atoi)
+
+This is a "norminette-like" approach to leetcode's atoi.
+
+The leetcode's atoi tests test extreme cases, even extrapolating the long long unsigned int reach.
+The code, therefore, needs to be prepared to meet the conditions in the description of the project,
+as well as the edge tests taken into account.
+
+This version is the most clean i could get by now.
---
+++ 8_atoi/src/ft_atoi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <limits.h>
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	i;
+	long int	num;
+
+	sign = 1;
+	i = 0;
+	while (s[i] == ' ')
+		i++;
+	if (s[i] == '-')
+	{
+		sign = -sign;
+		i++;
+	}
+	else if (s[i] == '+')
+		i++;
+	num = 0;
+	while (s[i])
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (sign * num);
+		num = (s[i] - '0') + (num * 10);
+		if ((num) >= INT_MAX)
+		{
+			if (sign > 0)
+				return (INT_MAX);
+			else if (sign < 0)
+				if (num > INT_MAX)
+					return (INT_MIN);
+				else
+					return ((int)(sign * num));
+		}
+		i++;
+	}
+	return (num * sign);
+}
